5a3b111827cb0e6b78d8989fcc018ebf66981b50
doctorRAZ <doctorRAZ@yandex.ru>
2025-02-13 06:54:06 +0300

    стоп UINSI, VP в отдельную библиотеку

--- PDFScaler/src/Program.cs
+++ PDFScaler/src/Program.cs
@@ -6,6 +6,7 @@ using drz.Infrastructure;
 using drz.Servise;
 
 using drz.PdfSharp.Pdf;
+using System.Linq;
 
 
 namespace drz.PDFScaler
@@ -31,9 +32,9 @@ namespace drz.PDFScaler
         {
             //загрузчик dll
             ReflectionLoader cmd = new ReflectionLoader();
-            Prog();
+            Prog(args);
         }
-        static void Prog()
+        static void Prog(string[] args)
         {
             ConsoleKey response;
 
@@ -42,6 +43,95 @@ namespace drz.PDFScaler
 
             //logger сообщения
             Logger = new List<Logger>();
+
+            //todo в отдельный класс
+            #region UINSI
+            /*
+            List<string> argsL = args.ToList();
+            List<string> Files = new List<string>();
+
+            for (int i = 0; i < argsL.Count; i++)
+            {
+                if (File.Exists(argsL[i]))
+                {
+                    if (Path.GetExtension(argsL[i]).ToLower() == ".pdf")
+                    {
+                        Files.Add(argsL[i]);
+                    }
+                    else if (Path.GetExtension(argsL[i]).ToLower() == ".png" || Path.GetExtension(argsL[i]).ToLower() == ".jpg" || Path.GetExtension(argsL[i]).ToLower() == ".jpeg")
+                    {
+                        Images.Add(argsL[i]);
+                    }
+                    else
+                    {
+                        LogHelper.Log($"The selected file is not a PDF or valid immage format (.png | .jpg | .jpeg), and will be excluded. {argsL[i]}", LogType.Warning);
+                    }
+                }
+                else if (Directory.Exists(argsL[i]))
+                {
+                    foreach (var item in Directory.EnumerateFiles(argsL[i]))
+                    {
+                        argsL.Add(item);
+                    }
+                }
+                else
+                {
+                    switch (argsL[i].ToLower())
+                    {
+                        case "-np":
+
+                            if (argsL.Count() > i + 1)
+                            {
+                                createNewPageFormat = argsL[i + 1];
+                                i++;
+                            }
+                            else
+                            {
+                                createNewPageFormat = "A4";
+                            }
+
+                            Config.ExitConfirmation = 0;
+
+                            break;
+
+                        case "-o":
+                            autoOpenFile = true;
+                            break;
+
+                        case "-b":
+                            PDFInterface.Bookmarks = 1;
+                            break;
+
+                        case "-s":
+                            splitAll = true;
+                            break;
+
+                        case "-flat":
+                            flat = true;
+                            break;
+
+                        case "-singlepagesplit":
+                            if (argsL.Count() >= i + 1)
+                            {
+                                bool r = Int32.TryParse(argsL[i + 1].ToLower(), out singlePageSplit);
+                                if (!r)
+                                {
+                                    singlePageSplit = 0;
+                                    break;
+                                }
+                            }
+                            i++;
+                            break;
+
+                        default:
+                            LogHelper.Log($"The argument option does not exist will be excluded. {argsL[i]}", LogType.Error);
+                            break;
+                    }
+                }
+            }
+
+            */
+            #endregion
 
             //приветственные сообщения
             CS.ConsoleWrite(MessagWelcom.Header, MesagType.Warning);
