bda79a46902781cc1178348aead82b888628a428
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-02-19 15:50:27 +0300

    added License info

--- PdfVPMod/src/Infrastructure/Setting.cs
+++ PdfVPMod/src/Infrastructure/Setting.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+*        Copyright doctorRAZ 2014-2025 by Разыграев Андрей
+*
+*        Licensed under the Apache License, Version 2.0 (the "License");
+*        you may not use this file except in compliance with the License.
+*        You may obtain a copy of the License at
+*
+*            http://www.apache.org/licenses/LICENSE-2.0
+*
+*        Unless required by applicable law or agreed to in writing, software
+*        distributed under the License is distributed on an "AS IS" BASIS,
+*        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*        See the License for the specific language governing permissions and
+*        limitations under the License.
+*/
+
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Xml;
@@ -39,7 +55,7 @@ namespace drz.PdfVpMod.Infrastructure
             "\t\t[Point] - точки/пойнты\n" +
             "\t\t[Inch] - дюймы\n" +
             "\t\t[Centimeter] - сантиметры\n" +
-            "\t\t[Presentation] - единицы презентации (не знаю, что это)")]
+            "\t\t[Presentation] - единицы презентации (1/96 дюйма)")]
         public WinGraphicsUnit Unit
         {
             get
